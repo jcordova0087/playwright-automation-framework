f4bfe7243da97a4c808ae0d3f82357cd3b5e74b6
JCordova <34633852+jcordova0087@users.noreply.github.com>
2026-07-04 13:08:13 -0700

    feat(pages): implement abstarct BasePage with clean barrel config imports

---
+++ src/pages/BasePage.ts
@@ -0,0 +1,64 @@
+import { Page, Locator } from '@playwright/test';
+import { Environment } from '../../config';
+
+/**
+ * Abstract base class representing the foundational contract for all page objects.
+ * Provides shared browser operations and enforces route management.
+ */
+
+export abstract class BasePage {
+    protected readonly page: Page;
+
+    /**
+     * The relative application path for the specific page (e.g., '/login').
+     * Must be implemnted by all derived Page Objects.
+     */
+
+    protected abstract readonly basePath: string;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    /**
+     * Navigates to the pages concrete URL by combining the BASE_URL and its basePath.
+     * @param appendPath appendPath Optional extra path parameters, query parameters, or anchors.
+     */
+    public async goto(appendPath: string = ''): Promise<void> {
+        const finalUrl = `${Environment.baseUrl}${this.basePath}${appendPath}`;
+        await this.page.goto(finalUrl);
+    }
+
+    /**
+     * Reloads the current page tracking the active session state
+     */
+    public async reload(): Promise<void> {
+        await this.page.reload();
+    }
+
+    /**
+     * Retrieves the current page's document title.
+     * @returns A promise that resolves to the page title string
+     */
+    public async getTitle(): Promise<string> {
+        return  await this.page.title();
+    }
+
+    /**
+     * Retrieves the exact URL currently loaded in the browser context.
+     * @returns  the full URl string.
+     */
+    public async getCurrentUtl(): Promise<string> {
+        return this.page.url();
+    }
+
+    /**
+     * Captures a screenshoot of the current viewport view for reporting purposes.
+     * @param screenshotName 
+     */
+    public async takeScreenshoot(screenshotName: string): Promise<void> {
+        await this.page.screenshot({path: `./screenshots/${screenshotName}-${Date.now()}.png`});
+    }
+
+}
+
