fec44dec10f047932d0095d2e27fff757cbea24b
JCordova <34633852+jcordova0087@users.noreply.github.com>
2026-07-04 11:52:41 -0700

    feat: add environment configuration

---
+++ config/Environment.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+/**
+ *  Provides centralized access to environment variables.
+ */
+
+export class Environment {
+    public static readonly baseUrl = Environment.getRequired('BASE_URL');
+
+    public static readonly apiUrl = Environment.getRequired('API_URL');
+
+    public static readonly username = Environment.getRequired('USERNAME');
+
+    public static readonly password = Environment.getRequired('PASSWORD');
+
+
+    /**
+     * Returns a required environemnt variable.
+     * Throws an error if the variable is missing.
+     * @param name 
+     * @returns 
+     */
+    private static getRequired(name: string): string {
+        const value = process.env[name];
+
+        if(!value) {
+            throw new Error(`Missing required environment variable: ${name}`);
+        }
+
+        return value;
+    }
+}
